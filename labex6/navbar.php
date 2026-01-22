cf61ab979ec11e1a17f8fb357fb5ac7848f4c0e5
aliya <aliya26205@gmail.com>
2026-01-22 20:39:16 +0530

    Added WT lab files

--- labex6/navbar.php
+++ labex6/navbar.php
@@ -5,7 +5,7 @@ $currentPage = basename($_SERVER['PHP_SELF']);
 <link rel="stylesheet" href="style.css">
 
 <div class="navbar">
-    <a href="index6.php" class="<?= ($currentPage == 'index.php') ? 'active' : '' ?>">Home</a>
+    <a href="index.php" class="<?= ($currentPage == 'index.php') ? 'active' : '' ?>">Home</a>
     <a href="students.php" class="<?= ($currentPage == 'students.php') ? 'active' : '' ?>">Students</a>
     <a href="companies.php" class="<?= ($currentPage == 'companies.php') ? 'active' : '' ?>">Companies</a>
     <a href="placements.php" class="<?= ($currentPage == 'placements.php') ? 'active' : '' ?>">Placements</a>
